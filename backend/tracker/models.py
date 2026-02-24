7703017b0ba44ae0adffef7c43a8b2a15aa2d0bc
Mahashwe <maha.tomholland@gmail.com>
2026-02-24 17:46:49 +0530

    changed backend models

--- backend/tracker/models.py
+++ backend/tracker/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Habit(models.Model):
     habitName = models.CharField(max_length=100)
     habitDescription = models.TextField()
-    frequency = models.CharField(max_length=1000)
+    frequency = models.IntegerField()
     done = models.BooleanField(default=False)
     last_updated = models.DateField(auto_now=True)
 
